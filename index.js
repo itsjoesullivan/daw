87e1af71e3fc5f444ddf99039c7176140a8aa34b
itsjoesullivan <itsjoesullivan@gmail.com>
2013-04-13 20:30:28 -0400

    playing catch-up here

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var sounds = {
 	's': 'demo/snare.wav'
 };
 
-var channelStrip = new ChannelStrip();
+var channelStrip = new ChannelStrip({
+	out: context.destination,
+	effects: [
+		//new Delay({ wet: .4, feedback: 0.4, time: .2}),
+		new Reverb({ wet: 0.4})
+	]
+});
 
 var drumMachine = new DrumMachine({
 	sounds: sounds,
-	out: channelStrip.input;
+	out: channelStrip.input
 });
-
