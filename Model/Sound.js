5e68dfe5f1019193459d29a167bf5861ee516e82
itsjoesullivan <itsjoesullivan@gmail.com>
2013-04-15 12:13:29 -0400

    generateSource provides a handle to a live bufferSource

--- Model/Sound.js
+++ Model/Sound.js
@@ -5,14 +5,44 @@ var Sound = function(conf,fn) {
 	this.load(conf.path);
 	this.out = conf.out;
 	this.ready = fn;
-}
+	this.sources = [];
+};
 
-Sound.prototype.play = function() {
+/** Spin off a buffer
+
+*/
+Sound.prototype.generateSource = function() {
 	var source = context.createBufferSource();
 	source.buffer = this.buffer;
 	source.connect(this.out);
-	source.noteOn(0);
+	return source;
 }
+
+Sound.prototype.play = function() {
+	this.schedule(context.currentTime);
+};
+
+Sound.prototype.schedule = function(when) {
+	if(! ('buffer' in this)) {
+		return false;
+	}
+	if(typeof when !== 'number' && !when) {
+		when = 0;
+	}
+	var source = this.generateSource();
+	source.noteOn(when);
+	this.sources.push({
+		when: when,
+		buffer: source
+	});
+};
+
+Sound.prototype.stop = function() {
+	this.sources.forEach(function(source) {
+		source.buffer.noteOff(source.when);
+	});
+	this.sources = [];
+};
 
 Sound.prototype.load = function(path,fn) {
 	var self = this;
@@ -28,4 +58,11 @@ Sound.prototype.load = function(path,fn) {
 		});
 	};
 	req.send();
+}
+
+
+if(typeof 'define' !== 'undefined') {
+	define(function() {
+		return Sound;
+	});
 }
